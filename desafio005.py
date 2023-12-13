78e4e9a375924bf85f028cc2a5a80707a60e84e0
antoniocarlosf <antoniocf1505@gmail.com>
2023-12-13 23:32:49 +0000

    Desafio Python

--- desafio005.py
+++ desafio005.py
@@ -1,11 +1,4 @@
-# n1 = int(input('Digite um numero: '))
-# s = n1 + 1
-# a = n1 - 1
-# print('O antecessor do seu numero é {} e o seu sucessor {}'.format(s, a))
-
 n1 = int(input('Digite um numero: '))
-d = n1 * 2
-t = n1 * 3
-r = n1 ** 1/2
-print('O Dobro deste numero é {} o triplo {} e a raiz quadrada {}'.format(d, t, r))
-
+s = n1 + 1
+a = n1 - 1
+print('O antecessor do seu numero é {} e o seu sucessor {}'.format(s, a))
---
+++ desafio008.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um valor em metros: '))
+c = n1 * 100
+m = n1 * 1000
+print('valor em metros {} \n O seu valor em centimetros é {}cm \n e em milimetros {}m'.format( n1, c, m))
