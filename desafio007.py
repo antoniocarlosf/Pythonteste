293116c22038f52b8d41c8468473a828dc8813fb
antoniocarlosf <antoniocf1505@gmail.com>
2023-12-13 22:21:54 +0000

    Desafio Python

---
+++ desafio007.py
@@ -0,0 +1,7 @@
+n1 = float(input('Primeira nota'))
+n2 = float(input('Segunda nota'))
+n3 = float(input('Terceira nota'))
+n4 = float(input('Quarta nota'))
+soma = n1 + n2 + n3 + n4
+media = soma / 5
+print('A media foi {:.1f}'.format(media))
