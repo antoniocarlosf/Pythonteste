ae2b11bb7ae0967687592e83a188f5cebe312705
antoniocarlosf <antoniocf1505@gmail.com>
2023-12-22 22:26:55 +0000

    Merge remote-tracking branch 'origin/Teste' into Teste

--- desafio033.py
+++ desafio033.py
@@ -2,7 +2,7 @@ a = int(input('Primeiro valor : '))
 b = int(input('Segundo valor : '))
 c = int(input('Terceiro valor : '))
 # Verificando quem é menor
-meno = a
+menor = a
 if b < a and b < c:
     menor = b
 if c < a and c < b:
